db39102ff4c9ce60cac477e3f2078351f1ccc208
Ankit <ankit.escalate360@gmail.com>
2020-09-19 18:40:06 +0530

    search a given word and select a particular word in window

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,55 +1,87 @@
 import React, { useState, useEffect } from 'react';
-import {
-  useQuery,
-  useQueryCache,
-  QueryCache,
-  ReactQueryCacheProvider,
-} from 'react-query';
-
-import axios from 'axios';
 import '../App.css';
-import { request, gql } from 'graphql-request';
+import { gql } from 'graphql-request';
+import { client } from '../graphqlClient';
+import { Link } from 'react-router-dom';
 
-const endpoint = 'http://localhost:8000/graphql';
-
-const queryCache = new QueryCache();
+const SEARCH_WORDS = gql`
+  query getWords($word: String!) {
+    searchWords(word: $word) {
+      _id
+      word
+    }
+  }
+`;
 
 const Home = () => {
   const [wordList, setwordList] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, seterror] = useState(false);
 
-  return (
-    <ReactQueryCacheProvider queryCache={queryCache}>
-      <p>hey</p>
-      {}
-    </ReactQueryCacheProvider>
-  );
+  const handleSearchInputChange = (e) => {
+    //console.log(e.target.value);
+    setSearchWord(e.target.value);
+    const variables = {
+      word: e.target.value,
+    };
 
-  function usePosts() {
-    return useQuery('posts', async () => {
-      const {
-        words: { data },
-      } = await request(
-        endpoint,
-        gql`
-          {
-            words {
-              _id
-              word
-              definitions {
-                etymologies
-                definitions
-                examples
-                lexicalCategory
-              }
-            }
+    client
+      .request(SEARCH_WORDS, variables)
+      .then((res) => {
+        setwordList(res.searchWords);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    const GET_WORDS = gql`
+      {
+        words {
+          _id
+          word
+          definitions {
+            etymologies
+            definitions
+            examples
+            lexicalCategory
           }
-        `
-      );
-      return data;
-    });
-  }
+        }
+      }
+    `;
+
+    client
+      .request(GET_WORDS)
+      .then((res) => setwordList(res.words))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return wordList && wordList.length ? (
+    <div>
+      <h1>Vocabulary App</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchWord}
+        onChange={handleSearchInputChange}
+      />
+      <ul>
+        {wordList.map((d) => {
+          return (
+            <li key={d._id}>
+              <Link to={`/card/${d._id}`}>{d.word}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
 };
 
 export default Home;
